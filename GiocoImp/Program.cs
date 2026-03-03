54c4ad1d3c9ddc2b6dafc946a00c292c83ed883f
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-03-03 20:01:25 +0100

    mod

--- GiocoImp/Program.cs
+++ GiocoImp/Program.cs
@@ -4,19 +4,66 @@
     {
         static void impiccato(string[] livello1, string[] livello2, string[] livello3)
         {
-            string parola = "";
+            string parola = "", lettera = "", p = "";
             Random rnd = new Random();
             Console.WriteLine("scegli il livello di difficoltà");
             Console.WriteLine("-------------------------------");
             Console.WriteLine("1/2/3");
-            Console.WriteLine("-------------------------------");
             int livello = Convert.ToInt32(Console.ReadLine());
             if (livello == 1)
             {
                 int rand = rnd.Next(1, 3);
                 if (rand == 1)
                 {
-                    livello1[0] = parola;
+                    parola = livello1[0];
+                    for(int i = 0;i< parola.Length; i++)
+                    {
+                        p = parola.Replace(parola[i], '_');
+                       
+                    }
+                    Console.WriteLine(p);
+                    Console.WriteLine();
+                    Console.WriteLine("Inserisci una lettera o vuoi provare ad indovinare la parola?(1/2)?");
+                    int scelta = Convert.ToInt32(Console.ReadLine());
+                    if(scelta == 1)
+                    {
+                        Console.WriteLine("Che lettera vuoi inserire?");
+                        lettera = Console.ReadLine();
+                        bool s = false;
+                        s = parola.Contains(lettera);
+                        if(s = true)
+                        {
+                            for(int i = 0; i< p.Length; i++)
+                            {
+                                int punto = p.IndexOf(lettera);
+                                p = parola.Replace(p[punto], lettera[0]);
+                            }
+                            Console.WriteLine(p);
+                        }
+                    }
+
+
+                }
+                else if (rand == 2)
+                {
+                    parola = livello1[1];
+                    for (int i = 0; i < parola.Length; i++)
+                    {
+                        parola = parola.Replace(parola[i], '_');
+
+                    }
+                    Console.WriteLine(parola);
+
+                }
+                else if(rand == 3)
+                {
+                    parola = livello1[2];
+                    for (int i = 0; i < parola.Length; i++)
+                    {
+                        parola = parola.Replace(parola[i], '_');
+
+                    }
+                    Console.WriteLine(parola);
 
                 }
             }
