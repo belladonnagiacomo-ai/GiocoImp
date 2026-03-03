e48644f5631449e35d89b16a7ab5416fe04f8d8e
belladonnagiacomo-ai <belladonna.giacomo@franchettisalviani.net>
2026-03-03 19:08:47 +0100

    mod

--- GiocoImp/Program.cs
+++ GiocoImp/Program.cs
@@ -2,9 +2,31 @@
 {
     internal class Program
     {
+        static void impiccato(string[] livello1, string[] livello2, string[] livello3)
+        {
+            string parola = "";
+            Random rnd = new Random();
+            Console.WriteLine("scegli il livello di difficoltà");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("1/2/3");
+            Console.WriteLine("-------------------------------");
+            int livello = Convert.ToInt32(Console.ReadLine());
+            if (livello == 1)
+            {
+                int rand = rnd.Next(1, 3);
+                if (rand == 1)
+                {
+                    livello1[0] = parola;
+
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] livello1 = { "cane", "albero", "telefono" };
+            string[] livello2 = { "orizzonte", "zainetto", "girasole" };
+            string[] livello3 = { "squarcio", "ziggurat", "alchimia", "osmosi" };
+            impiccato(livello1, livello2, livello3);
         }
     }
 }
